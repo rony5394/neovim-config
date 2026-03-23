f9276fba16b7462b64ba16f593532d530951df40
rony5394 <143897221+rony5394@users.noreply.github.com>
2026-03-23 14:34:29 +0100

    Something Untested.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,24 +33,24 @@ return {
       })
 
 
-	require("lspconfig").omnisharp.setup {
-		on_attach = on_attach,
-		cmd = {
-			"dotnet",
-			"/home/rony539/local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll",
-			"--languageserver",
-			"--hostPID", tostring(vim.fn.getpid())
-		},
-		settings = {
-			FormattingOptions = {
-				EnableEditorConfigSupport = false,
-				OrganizeImports = true,
-			},
-			Sdk = {
-				IncludePrereleases = true,
-			},
-		},
-	}
+	-- require("lspconfig").omnisharp.setup {
+	-- 	on_attach = on_attach,
+	-- 	cmd = {
+	-- 		"dotnet",
+	-- 		"/home/rony539/local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll",
+	-- 		"--languageserver",
+	-- 		"--hostPID", tostring(vim.fn.getpid())
+	-- 	},
+	-- 	settings = {
+	-- 		FormattingOptions = {
+	-- 			EnableEditorConfigSupport = false,
+	-- 			OrganizeImports = true,
+	-- 		},
+	-- 		Sdk = {
+	-- 			IncludePrereleases = true,
+	-- 		},
+	-- 	},
+	-- }
 
     end,
   },
