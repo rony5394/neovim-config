116cf9d7bf8367b4fecc1107352efd3af2b27f2d
rony5394 <143897221+rony5394@users.noreply.github.com>
2026-03-27 13:23:12 +0100

    Some lsp stuff.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,98 +1,91 @@
 return {
-  -- Core LSP config
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- Define on_attach function with LSP keymaps
-      local on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        local keymap = vim.keymap.set
+	-- Core LSP config
+	{
+	"neovim/nvim-lspconfig",
+	config = function()
+		-- Define on_attach function with LSP keymaps
+		local on_attach = function(client, bufnr)
+			local opts = { noremap = true, silent = true, buffer = bufnr }
+			local keymap = vim.keymap.set
 
-        keymap("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition", buffer = bufnr })
-        keymap("n", "K", vim.lsp.buf.hover, { desc = "Hover", buffer = bufnr })
-        keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename", buffer = bufnr })
-        keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", buffer = bufnr })
-        keymap("n", "<leader>gr", vim.lsp.buf.references, { desc = "References", buffer = bufnr })
-        keymap("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Implementation", buffer = bufnr })
-	keymap("n", "<leader>d", vim.diagnostics.open_float, { desc = "Show Error", buffer = bufnr}) 
-      end
+			keymap("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to Definition", buffer = bufnr })
+			keymap("n", "K", vim.lsp.buf.hover, { desc = "Hover", buffer = bufnr })
+			keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename", buffer = bufnr })
+			keymap("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action", buffer = bufnr })
+			keymap("n", "<leader>gr", vim.lsp.buf.references, { desc = "References", buffer = bufnr })
+			keymap("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Implementation", buffer = bufnr })
+			keymap("n", "<leader>G", vim.diagnostic.open_float, { desc = "Show Error", buffer = bufnr}) 
+		end
 
-      -- Save globally to reuse in mason-lspconfig
-      vim.g._lsp_on_attach = on_attach
+		-- Save globally to reuse in mason-lspconfig
+		vim.g._lsp_on_attach = on_attach
 
-      -- Manual setup example: Lua language server
-      require("lspconfig").lua_ls.setup({
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
-
-
-	require("lspconfig").omnisharp.setup {
-		on_attach = on_attach,
-		cmd = {
-			"/run/current-system/sw/bin/OmniSharp",
-			"--stdio"
-			-- "dotnet",
-			-- "/home/rony539/local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll",
-			-- "--languageserver",
-			-- "--hostPID", tostring(vim.fn.getpid())
-		},
-		settings = {
-			FormattingOptions = {
-				EnableEditorConfigSupport = false,
-				OrganizeImports = true,
+		require("lspconfig").omnisharp.setup {
+			on_attach = on_attach,
+			cmd = {
+				"/run/current-system/sw/bin/OmniSharp",
+				"--stdio"
+				-- "dotnet",
+				-- "/home/rony539/local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll",
+				-- "--languageserver",
+				-- "--hostPID", tostring(vim.fn.getpid())
 			},
-			Sdk = {
-				IncludePrereleases = true,
+			settings = {
+				FormattingOptions = {
+					EnableEditorConfigSupport = false,
+					OrganizeImports = true,
+				},
+				Sdk = {
+					IncludePrereleases = true,
+				},
 			},
-		},
-	}
+		}
 
-    end,
-  },
+	end,
+	},
 
-  -- Mason: installs LSP servers
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	-- Mason: installs LSP servers
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- Mason-LSPConfig: connects Mason and LSPConfig
-  {
-    "williamboman/mason-lspconfig.nvim",
-    version = "1.32.0", -- pin to a stable version if desired
-    dependencies = { "mason.nvim", "nvim-lspconfig" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "intelephense",
-	  "gopls",
-	  -- "omnisharp"
-        },
-        automatic_installation = true,
-      })
+	-- Mason-LSPConfig: connects Mason and LSPConfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		version = "1.32.0", -- pin to a stable version if desired
+		dependencies = { "mason.nvim", "nvim-lspconfig" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"gopls",
+					"intelephense",
+					"lua_ls",
+					"phpactor",
+					-- "phpcs",
+					"pylsp",
+					-- "snyk",
+					"svelte",
+					"ts_ls",
+					-- "omnisharp",
+				},
+				automatic_installation = true,
+			})
 
-      local lspconfig = require("lspconfig")
-      local on_attach = vim.g._lsp_on_attach
+			local lspconfig = require("lspconfig")
+			local on_attach = vim.g._lsp_on_attach
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup({
-            on_attach = on_attach,
-          })
-        end,
-      })
-    end,
-  },
+			require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({
+					on_attach = on_attach,
+				})
+			end,
+			})
+		end,
+	},
 }
 
